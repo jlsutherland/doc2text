5e56bf8ad33b9061ad46fa640ed626ae52df6a65
Joe Sutherland <jls124@icloud.com>
2016-08-28 17:57:08 -0400

    deps update, tox fix

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['PyPDF2', 'numpy', 'PIL', 'scipy', 'pytesseract', 'PythonMagick'],
+    install_requires=['PyPDF2', 'numpy', 'Pillow', 'scipy', 'pytesseract', 'PythonMagick'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
