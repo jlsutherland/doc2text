a6e3857f0eaedaf894134486d422480f1a246c72
Joe Sutherland <jls124@icloud.com>
2016-08-29 11:25:26 -0400

    update init

--- doc2text/__init__.py
+++ doc2text/__init__.py
@@ -1,6 +1,25 @@
 import PyPDF2 as pyPdf
+import os
 from .page import Page
 
 def main():
     """Entry point for the application script"""
     print("Call your main application code here")
+
+def Document:
+    def __init__(self, path):
+        self.file_ext = os.path.basename(path).split('.')[1]
+        # see if it is a pdf
+        # if it's a pdf, then peel all of the pages off
+        # for each page, perform the basic operation
+        # have a save function to save the text from the pages
+        # or the converted pages themselves.
+
+    def process(self):
+        stuff = stuff
+
+    def get_text(self):
+        stuff = stuff
+
+    def save_pages(self):
+        stuff = stuff
