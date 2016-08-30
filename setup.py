8d86f17d8956217a1b2371bc521136e2d83e942b
Joe Sutherland <jls124@icloud.com>
2016-08-29 20:18:12 -0400

    update package deps to include mimetype

--- setup.py
+++ setup.py
@@ -51,13 +51,8 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.5.3',
     ],
 
     # What does your project relate to?
@@ -75,7 +70,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['PyPDF2', 'numpy', 'Pillow', 'scipy', 'pytesseract'],
+    install_requires=['PyPDF2', 'numpy', 'Pillow', 'scipy', 'pytesseract', 'mimetypes'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
