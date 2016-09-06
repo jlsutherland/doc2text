bc32bb11822fecb7cfdf53d4a253a5c46b96f4e0
Joe Sutherland <jls124@icloud.com>
2016-09-06 17:58:17 -0400

    bump to build with lang var support

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='doc2text',
-    version='0.2.3',
+    version='0.2.4',
 
     description='doc2text drastically improves the extraction of text from images by fixing resolution, text area (crop), and skew.',
     long_description=long_description,
